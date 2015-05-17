8c3c78d9d1c9bb2d930a21ab0ad06d0900d78f6c
Aristeides Stathopoulos <aristath@gmail.com>
2015-05-17 15:53:56 +0300

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var $container = $("#works-container").imagesLoaded( function() {
+    $container.packery();
+    // BUG: when the container is clicked, the image is revealed and therefore we no longer click on the same element.
+    // As a result, items with the "gigante" class stay big no matter what.
+    $container.on( "click", ".item", function( event ) {
+        // change size of item via class
+        $( event.target ).toggleClass("gigante");
+        // trigger layout
+        $container.packery();
+    });
+});
